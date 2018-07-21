b9d14a79f8086d071d67a93cc42d16dc955c0c8a
Nikola-Popov <nikola.angelov.popov@gmail.com>
2018-07-21 15:08:04 +0300

    fix transaction brojer module setup

--- transaction-broker/src/main/java/io/broker/transaction/TransactionBrokerApplication.java
+++ transaction-broker/src/main/java/io/broker/transaction/TransactionBrokerApplication.java
@@ -1,9 +1,11 @@
+package io.broker.transaction;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlockchainClientApplication {
+public class TransactionBrokerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BlockchainClientApplication.class, args);
+		SpringApplication.run(TransactionBrokerApplication.class, args);
 	}
 }
