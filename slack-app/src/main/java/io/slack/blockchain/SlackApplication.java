4664d7d399d5b4daf6de81f520c67ffbdcdd9182
Nikola-Popov <nikola.angelov.popov@gmail.com>
2018-08-14 19:44:59 +0300

    Fix project setup.Removed POM spring-maven plugin

--- slack-app/src/main/java/io/slack/blockchain/SlackApplication.java
+++ slack-app/src/main/java/io/slack/blockchain/SlackApplication.java
@@ -3,7 +3,7 @@ package io.slack.blockchain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackageClasses = io.slack.blockchain.commons.services.GsonJsonService.class)
+@SpringBootApplication
 public class SlackApplication {
 
 	public static void main(String[] args) {
