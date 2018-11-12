9bca9f138141420507339316145fff45d6abe162
Nikola-Popov <nikola.angelov.popov@gmail.com>
2018-11-13 00:58:41 +0200

    CoinbaseAuthorization controller tests added

---
+++ transaction-broker/src/test/java/io/slack/blockchain/coinbase/security/oauth/controllers/CoinbaseAuthorizationHandlerRestControllerTest.java
@@ -0,0 +1,44 @@
+package io.slack.blockchain.coinbase.security.oauth.controllers;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.slack.blockchain.coinbase.security.oauth.exceptions.AuthorizationException;
+import io.slack.blockchain.coinbase.security.oauth.services.AuthorizationService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CoinbaseAuthorizationHandlerRestControllerTest {
+	private static final String ACCESS_TOKEN = "accessToken";
+	private static final String CODE = "code";
+	private static final String STATE = "state";
+
+	@Autowired
+	@InjectMocks
+	private CoinbaseAuthorizationHandlerRestController coinbaseAuthorizationHandlerRestController;
+
+	@Mock
+	private AuthorizationService authorizationServiceMock;
+
+	@Test(expected = AuthorizationException.class)
+	public void testAuthorizeAcquiringAccessTokenFails() throws Exception {
+		when(authorizationServiceMock.acquireAccessTokenConsumingCode(CODE)).thenThrow(AuthorizationException.class);
+
+		coinbaseAuthorizationHandlerRestController.authorize(CODE, STATE);
+	}
+
+	@Test
+	public void testAuthorize() throws Exception {
+		when(authorizationServiceMock.acquireAccessTokenConsumingCode(CODE)).thenReturn(ACCESS_TOKEN);
+
+		coinbaseAuthorizationHandlerRestController.authorize(CODE, STATE);
+
+		verify(authorizationServiceMock).acquireAccessTokenConsumingCode(CODE);
+	}
+}
