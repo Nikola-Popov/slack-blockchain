40049ff723a08322fa8159e500c3e24d2cb3b0f7
Nikola-Popov <nikola.angelov.popov@gmail.com>
2018-11-26 00:59:06 +0200

    Fix processor bean loading issue

--- slack-app/src/main/java/io/slack/blockchain/processing/dialog/TransactionDialogProcessor.java
+++ slack-app/src/main/java/io/slack/blockchain/processing/dialog/TransactionDialogProcessor.java
@@ -1,8 +1,11 @@
 package io.slack.blockchain.processing.dialog;
 
+import org.springframework.stereotype.Component;
+
 import io.slack.blockchain.domain.dialog.contents.TransactionDialogContent;
 import io.slack.blockchain.domain.processing.ProcessingResult;
 
+@Component
 public class TransactionDialogProcessor implements DialogProcessor<TransactionDialogContent> {
 
 	@Override
